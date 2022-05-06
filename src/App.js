3daf9dae0aa1dd1ac8a369beb7a74b76c4589df4
Mariusz Lewandowski <mar.lewandowski@gmail.com>
2022-05-06 12:23:47 +0200

    add lorem that do nothing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim hic molestias neque placeat sunt? Accusamus atque commodi cumque dolore facere fuga libero nobis rem sapiente vel. Asperiores enim et mollitia.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
